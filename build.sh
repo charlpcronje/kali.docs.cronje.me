d133d9f51c1002f6028194ab32c97caf0d8beb2a
Charl Cronje <charlcp@gmail.com>
2022-09-20 18:09:32 +0200

    Update docs

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-retype build --host kali.docs.cronje.ME
+#!/bin/bash
+if [ ! -d "./public" ] 
+then 
+    echo "Directory './public' does not exist";
+    echo "Creating Directory: './public'";
+fi
+  
+echo "Running Build";
+retype build
+cd public
+find . -name "*.html" -exec sed -i 's/m = localStorage.getItem("doc_theme")/m = "dark"/g' {} \;
+cd ..
